edcc0e17ba5660e67772b91da5aca4f6d31e8148
Gabriel Solis Corrêa <gabri.ell@hotmail.com>
2018-10-08 20:17:55 -0300

    Add. esqueleto do Maquinas de estados do Boss de TI

---
+++ Assets/Scripts/Enemy/Bosses/TI/TIBossBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TIBossBehavior : MonoBehaviour {
+
+	public TIBaseState MyState;
+
+	void Start () {
+		MyState = new TIGenius();
+	}
+
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/Enemy/Bosses/TI/TILogicGame.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TILogicGame : TIBaseState {
+
+	void Update () {
+
+	}
+}
