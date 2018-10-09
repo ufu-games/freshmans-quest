ba219cd0654f0440763fc37aca2d3f6070476331
Gabriel Solis Corrêa <gabri.ell@hotmail.com>
2018-10-09 14:38:28 -0300

    Mudanças no TIBossBehavior e TILogicGame

--- Assets/Scripts/Enemy/Bosses/TI/TIBossBehavior.cs
+++ Assets/Scripts/Enemy/Bosses/TI/TIBossBehavior.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class TIBossBehavior : MonoBehaviour {
 
+	public GameObject[] Buttons;
 	public TIBaseState MyState;
+	public int HP = 3;
 
 	void Start () {
 		MyState = new TIGenius();
 	}
 
 	void Update () {
-		
+
+	}
+
+	public void ButtonActivate(int i, string buttonlabel){
+
+	}
+
+	public void ButtonDeactivate(int i){
+
+	}
+
+	public void DealDMGBoss(int dmg){
+		if(HP - dmg <= 0){
+			HP = 0;
+		} else {
+			HP -= dmg;
+		}
+		//GetComponent<Animator>().Play("TITakeDamage");
+	}
+
+	public void DealDMGPlayer(int dmg){
+		GameObject.Find("Player").GetComponent<HealthManager>().TakeDamage((float) dmg);
 	}
 }
