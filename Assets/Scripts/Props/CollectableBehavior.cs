ad8f09356d7d1577da8d99048ef5bb6c13385d3d
Guilherme Oliveira <guilherme.061@gmail.com>
2018-11-22 14:42:30 -0200

    destroy collectable coroutine

--- Assets/Scripts/Props/CollectableBehavior.cs
+++ Assets/Scripts/Props/CollectableBehavior.cs
@@ -6,16 +6,37 @@ public class CollectableBehavior : MonoBehaviour, IInteractable {
 
 	public float Value;
 	public TypeOfCollectable Type;
+	public AudioClip collectedClip;
+	private float followTime = .65f;
+	private float smoothFollow = 2f;
 
 	public enum TypeOfCollectable{Pizza, Homework};
 
+	private IEnumerator DestroyCollectableRoutine(PlayerController playerReference) {
+		float timeSpent = 0;
+
+		while(timeSpent < followTime) {
+			Vector3 tempPosition = playerReference.transform.position;
+			timeSpent += Time.deltaTime;
+			yield return null;
+			transform.position = Vector3.Lerp(transform.position, tempPosition, Time.deltaTime * smoothFollow);
+		}
+
+		if(collectedClip && SoundManager.instance) {
+			SoundManager.instance.PlaySfx(collectedClip);
+		}
+		Destroy(gameObject);
+	}
+
 	public void Interact(){
+		PlayerController t_playerControllerReference = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
 		if(Type == TypeOfCollectable.Pizza) {
-			GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().PizzaCollected += Value;
+			t_playerControllerReference.PizzaCollected += Value;
 		}
 		if(Type == TypeOfCollectable.Homework) {
-			GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().HomeworkCollected += Value;
+			t_playerControllerReference.HomeworkCollected += Value;
 		}
-		Destroy(this.gameObject);
+		
+		StartCoroutine(DestroyCollectableRoutine(t_playerControllerReference));
 	}
 }
