e3c77bbfa45abedfbe8b0b7c54cd38e55d533843
Guilherme Oliveira <guilherme.061@gmail.com>
2018-12-01 00:37:15 -0200

    novo hub (agora vai)

---
+++ Assets/Scripts/Utilities/FloatWithSin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatWithSin : MonoBehaviour {
+	
+	Vector3 originalPosition;
+	float angle = 0f;
+
+	void Start() {
+		originalPosition = transform.position;
+	}
+
+	void Update() {
+		angle += Time.deltaTime;
+		transform.position = new Vector3(originalPosition.x, originalPosition.y + (Mathf.Sin(angle) / 8), originalPosition.z);
+	}
+}
