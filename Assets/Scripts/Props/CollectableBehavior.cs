ce1375fae67b07a39d19c645f9d5c6df2d211920
Gabriel Solis Corrêa <gabri.ell@hotmail.com>
2018-12-04 12:05:00 -0200

    Novo follow do coletavel

--- Assets/Scripts/Props/CollectableBehavior.cs
+++ Assets/Scripts/Props/CollectableBehavior.cs
@@ -8,8 +8,13 @@ public class CollectableBehavior : MonoBehaviour, IInteractable {
 	public TypeOfCollectable Type;
 	public AudioClip collectedClip;
 	public AudioClip continuousClip;
-	private float followTime = .65f;
-	private float smoothFollow = 2f;
+	[Header("Follow")]
+	[Range(0,100)]
+	public float DampingFirstMove = 90;
+	public float InitialVelocityFirstMove = 10;
+	public float AccelerationSecondMove = 1.25f;
+	public float InitialVelocitySecondMove = 3;
+	private bool StartedFollowing = false;
 
 	public enum TypeOfCollectable{Pizza, Homework};
 
@@ -24,15 +29,47 @@ public class CollectableBehavior : MonoBehaviour, IInteractable {
 	}
 
 	private IEnumerator DestroyCollectableRoutine(PlayerController playerReference) {
-		float timeSpent = 0;
-
-		while(timeSpent < followTime) {
-			Vector3 tempPosition = playerReference.transform.position;
-			timeSpent += Time.deltaTime;
+		float angleBetweenCollandPlayer = Mathf.Rad2Deg * Mathf.Atan2(transform.position.y - playerReference.transform.position.y,transform.position.x - playerReference.transform.position.x);
+		float randomAngle = (angleBetweenCollandPlayer + Random.Range(-30f,30f));
+		if(randomAngle < 0) {
+			randomAngle += 360;
+		}
+		if(randomAngle > 360) {
+			randomAngle -= 360;
+		}
+		Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+		if(rb == null) {
+			print("Falta Rigidbody no Coletável " + name);
+			Destroy(gameObject);
+		}
+		rb.velocity = (new Vector3(Mathf.Cos(Mathf.Deg2Rad*randomAngle),Mathf.Sin(Mathf.Deg2Rad*randomAngle),0))*InitialVelocityFirstMove;
+		
+		//---------- O coletável é jogado numa direção aleatória e começa a freiar
+		
+		while(Mathf.Abs(rb.velocity.magnitude) > 0.05f) {
 			yield return null;
-			transform.position = Vector3.Lerp(transform.position, tempPosition, Time.deltaTime * smoothFollow);
+			rb.velocity *= DampingFirstMove/100;
 		}
 
+		//---------- Depois de frear ele começa a ir rapidamente para o player
+
+		yield return new WaitForSeconds(0.02f);
+		float increasingVel = 0;
+		while(Vector3.Distance(transform.position,playerReference.transform.position) > 0.5f) {
+			increasingVel += AccelerationSecondMove * Time.deltaTime;
+			transform.position = Vector3.Lerp(transform.position, playerReference.transform.position, increasingVel);
+			yield return null;
+		}
+
+		//---------- O Coletável Chegou no player
+
+		if(Type == TypeOfCollectable.Pizza) {
+			playerReference.PizzaCollected += Value;
+			playerReference.UpdatePizzaCounter();
+		}
+		if(Type == TypeOfCollectable.Homework) {
+			playerReference.HomeworkCollected += Value;
+		}
 		if(collectedClip && SoundManager.instance) {
 			SoundManager.instance.PlaySfx(collectedClip);
 		}
@@ -40,15 +77,10 @@ public class CollectableBehavior : MonoBehaviour, IInteractable {
 	}
 
 	public void Interact(){
-		PlayerController t_playerControllerReference = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-		if(Type == TypeOfCollectable.Pizza) {
-			t_playerControllerReference.PizzaCollected += Value;
-			t_playerControllerReference.UpdatePizzaCounter();
+		if(!StartedFollowing) {
+			StartedFollowing = true;
+			PlayerController t_playerControllerReference = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+			StartCoroutine(DestroyCollectableRoutine(t_playerControllerReference));
 		}
-		if(Type == TypeOfCollectable.Homework) {
-			t_playerControllerReference.HomeworkCollected += Value;
-		}
-		
-		StartCoroutine(DestroyCollectableRoutine(t_playerControllerReference));
 	}
 }
