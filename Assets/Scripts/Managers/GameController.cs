de538f3d72fd033d87a5323b7032af95e8c5dcd5
Guilherme Oliveira <guilherme.061@gmail.com>
2019-01-17 23:33:16 -0200

    Tela de Pause simples

---
+++ Assets/Scripts/Managers/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    private bool m_isPaused = false;
+    public GameObject pausePanel;
+
+    void Start() {
+        m_isPaused = false;
+        pausePanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(InputManager.instance.PressedConfirm()) {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause() {
+        m_isPaused = !m_isPaused;
+        Time.timeScale = m_isPaused ? 0 : 1;
+        pausePanel.SetActive(m_isPaused);
+    }
+}
