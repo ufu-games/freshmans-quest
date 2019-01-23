17d7b2237974626aa7dc33b59cdf5389fed037f4
Barbara Franco <bah1sfc@gmail.com>
2019-01-22 22:58:31 -0200

    Lvl da universidade

---
+++ Assets/Scripts/KillBehavior.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class KillBehavior : MonoBehaviour, IInteractable
+{
+    public void Interact() {
+        FindObjectOfType<CheckpointSystemBehavior>().ResetPlayer();
+    }
+}
