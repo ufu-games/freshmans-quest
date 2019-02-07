5963a27f27e0691c4ae50f1f7b12218f81f9908c
Guilherme Oliveira <guilherme.061@gmail.com>
2019-02-07 19:16:35 -0200

    script para um objeto do cenario se mover com o tempo

---
+++ Assets/Scripts/Utilities/MoveOverTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOverTime : MonoBehaviour {
+    [Range(1,100)]
+    public float movingDistance = 5f;
+    [Range(0,1)]
+    public float movingVelocity = 0.1f;
+    private Vector3 m_initialPosition;
+
+    void Start() {
+        m_initialPosition = this.transform.position;
+    }
+
+    void Update() {
+        Vector3 tempPosition = m_initialPosition;
+        tempPosition.x += Mathf.Sin(Time.time * movingVelocity) * movingDistance;
+
+        this.transform.position = tempPosition;
+    }
+}
