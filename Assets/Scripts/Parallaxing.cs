6a37bbcb808c65046eab9449c17e88beeadc2c30
Guilherme Oliveira <guilherme.061@gmail.com>
2019-02-07 18:57:42 -0200

    agora o parallax funciona em todos objetos na Tag "Parallax"

--- Assets/Scripts/Parallaxing.cs
+++ Assets/Scripts/Parallaxing.cs
@@ -10,7 +10,7 @@ public class Parallaxing : MonoBehaviour {
     public float parallaxAmount = 1f; 
 
     /* Array of elements that are going to be Parallaxed */
-    public Transform[] backgrounds;
+    private Transform[] m_backgrounds;
     /* Proportion of Camera's movement to move backgrounds */
     private float[] parallaxScales;
 
@@ -19,30 +19,36 @@ public class Parallaxing : MonoBehaviour {
 
     void Awake() {
         m_cameraReference = Camera.main.transform;
+        var tempBackgrounds = GameObject.FindGameObjectsWithTag("Parallax");
+        m_backgrounds = new Transform[tempBackgrounds.Length];
+
+        for(int i = 0; i < tempBackgrounds.Length; i++) {
+            m_backgrounds[i] = tempBackgrounds[i].transform;
+        }
     }
 
     void Start() {
         m_previousCameraPosition = m_cameraReference.position;
 
-        parallaxScales = new float[backgrounds.Length];
+        parallaxScales = new float[m_backgrounds.Length];
 
-        for(int i = 0; i < backgrounds.Length; i++) {
-            parallaxScales[i] = backgrounds[i].position.z * - 1;
+        for(int i = 0; i < m_backgrounds.Length; i++) {
+            parallaxScales[i] = m_backgrounds[i].position.z * - 1;
         }
     }
 
     void Update() {
-        for(int i = 0; i < backgrounds.Length; i++) {
+        for(int i = 0; i < m_backgrounds.Length; i++) {
             /* The Parallax is the opposite of the camera movement */
             float parallax = (m_previousCameraPosition.x - m_cameraReference.position.x) * parallaxScales[i];
 
             /* set the target x position, which is the current position + parallax */
-            float backgroundTargetPositionX = backgrounds[i].position.x + parallax;
+            float backgroundTargetPositionX = m_backgrounds[i].position.x + parallax;
 
-            Vector3 backgroundTargetPosition = new Vector3(backgroundTargetPositionX, backgrounds[i].position.y, backgrounds[i].position.z);
+            Vector3 backgroundTargetPosition = new Vector3(backgroundTargetPositionX, m_backgrounds[i].position.y, m_backgrounds[i].position.z);
 
             /* Lerp between current position and the target position */
-            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPosition, parallaxAmount * Time.deltaTime);
+            m_backgrounds[i].position = Vector3.Lerp(m_backgrounds[i].position, backgroundTargetPosition, parallaxAmount * Time.deltaTime);
         }
 
         m_previousCameraPosition = m_cameraReference.position;
