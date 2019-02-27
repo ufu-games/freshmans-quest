0be9067bf0a66e0d9a72522ee1de0d0e67fcb78f
Tiago Pereira de Faria <tiago_pereira_faria@hotmail.com>
2019-02-26 22:05:11 -0300

    Enemy

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public enum Direction {Right, Left, Stoped};
+
+    public Direction InitialDirection;
+    public float speed;
+    public bool FollowPlayer;
+    public float MinDistanceToFollowPlayer;
+
+    private Rigidbody2D rb;
+    private float realDirection;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+        if(InitialDirection == Direction.Right)realDirection = 1;
+        if(InitialDirection == Direction.Left)realDirection = -1;
+        if(InitialDirection == Direction.Stoped)realDirection = 0;
+
+        player = GameObject.Find("Player");
+
+    }
+    public float getDirection(){
+        return realDirection;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        var newPosition = transform.position;
+        var playerPos = player.GetComponent<Transform>().position;
+        if(FollowPlayer == true && Mathf.Abs(transform.position.x - playerPos.x) < MinDistanceToFollowPlayer){
+            if(transform.position.x > playerPos.x ){
+                if(transform.position.x - speed > playerPos.x)
+                    newPosition.x -= speed;
+                else
+                    newPosition.x = playerPos.x;             
+            } else {
+                if(transform.position.x + speed < playerPos.x)
+                    newPosition.x += speed;
+                else
+                    newPosition.x = playerPos.x; 
+            }
+            
+        } else {
+            newPosition.x += this.realDirection * speed;
+        }
+            transform.position = newPosition;
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.layer == LayerMask.NameToLayer("PlayerRedirector")){
+            this.realDirection = col.gameObject.GetComponent<EnemyRedirectioner>().getAngle();
+        }
+    }
+}
---
+++ Assets/EnemyRedirectioner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRedirectioner : MonoBehaviour
+{
+    public enum Direction {Right, Left, Stop};
+    
+    public Direction direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public float getAngle(){
+        if(direction == Direction.Right)return 1;
+        if(direction == Direction.Left)return -1;
+        if(direction == Direction.Stop)return 0;
+        return 0;
+    }
+}
