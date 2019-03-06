e76c19d655ffc2d69f27ea5780cf885a73688fe5
Guilherme Oliveira <guilherme.061@gmail.com>
2019-03-06 11:36:37 -0300

    fix na falling platform

--- Assets/Scripts/Props/FallingPlatform.cs
+++ Assets/Scripts/Props/FallingPlatform.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour, ICollisionInteraction, IResettableProp {
     public float timeUntilFall = 3f;
+    public float alphaToDeactivateCollision = 0.3f;
     public float timeUntilReappear = 3f;
     public float reappearAnimationTime = 1f;
+    public float alphaToReactivateCollision = 0.7f;
 
     private SpriteRenderer m_spriteRenderer;
     private BoxCollider2D m_boxCollider;
@@ -25,6 +27,12 @@ public class FallingPlatform : MonoBehaviour, ICollisionInteraction, IResettable
         while(timeElapsed < reappearAnimationTime) {
             timeElapsed += Time.deltaTime;
             Color tempColor = m_spriteRenderer.color;
+            
+            if(tempColor.a >= alphaToReactivateCollision) {
+                m_boxCollider.enabled = true;
+                m_canBeInteractWith = true;
+            }
+
             tempColor.a = Mathf.Lerp(0, 1, (timeElapsed/reappearAnimationTime));
             m_spriteRenderer.color = tempColor;
             yield return null;
@@ -35,8 +43,6 @@ public class FallingPlatform : MonoBehaviour, ICollisionInteraction, IResettable
         m_spriteRenderer.color = t_color;
         yield return null;
 
-        m_boxCollider.enabled = true;
-        m_canBeInteractWith = true;
     }
 
     private IEnumerator FallPlatformRoutine() {
@@ -46,10 +52,16 @@ public class FallingPlatform : MonoBehaviour, ICollisionInteraction, IResettable
         while(timeElapsed < timeUntilFall) {
             timeElapsed += Time.deltaTime;
             Color tempColor = m_spriteRenderer.color;
+            
+            if(tempColor.a <= alphaToDeactivateCollision) {
+                m_boxCollider.enabled = false;
+            }
+
             tempColor.a = Mathf.Lerp(1, 0, (timeElapsed / timeUntilFall));
             m_spriteRenderer.color = tempColor;
             yield return null;
         }
+
 
         Color t_color = m_spriteRenderer.color;
         t_color.a = 0;
@@ -57,7 +69,6 @@ public class FallingPlatform : MonoBehaviour, ICollisionInteraction, IResettable
         yield return null;
 
         m_spriteRenderer.enabled = false;
-        m_boxCollider.enabled = false;
         StartCoroutine(ReappearPlatformRoutine());
     }
     void ICollisionInteraction.Interact() {
