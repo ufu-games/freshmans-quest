6d1d30fcad9d6cb85b84f64c028117db1d4456d3
Tiago Pereira de Faria <tiago_pereira_faria@hotmail.com>
2019-03-26 21:10:08 -0300

    Inimigo que cai

---
+++ Assets/EnemyThatFallsBehaviour.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyThatFallsBehaviour : MonoBehaviour
+{
+     public enum Direction {Right, Left, Stoped};
+
+    public Direction InitialDirection;
+    public float speed;
+    public bool FollowPlayer;
+    public float MinDistanceToFollowPlayer;
+
+    private Rigidbody2D rb;
+    private float realDirection;
+    private Vector2 initialPosition;
+    private GameObject player;
+    /// <summary>
+	/// mask with all layers that the player should interact with
+	/// </summary>
+    public LayerMask platformMask = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.initialPosition = transform.position;
+
+        if(InitialDirection == Direction.Right)realDirection = 1;
+        if(InitialDirection == Direction.Left)realDirection = -1;
+        if(InitialDirection == Direction.Stoped)realDirection = 0;
+
+        player = GameObject.Find("Player");
+
+    }
+    public float getDirection(){
+        return realDirection;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newPosition = transform.position;
+        var playerPos = player.GetComponent<Transform>().position;
+        if(FollowPlayer == true && Mathf.Abs(transform.position.x - playerPos.x) < MinDistanceToFollowPlayer) {
+            if(transform.position.x > playerPos.x ){
+                if(transform.position.x - speed > playerPos.x)
+                    newPosition.x -= speed;
+                else
+                    newPosition.x = playerPos.x;             
+            } else {
+                if(transform.position.x + speed < playerPos.x)
+                    newPosition.x += speed;
+                else
+                    newPosition.x = playerPos.x; 
+            }
+
+            UpdateLocalScale(transform.position - playerPos);
+        } else {
+            newPosition.x += this.realDirection * speed;
+            // invertendo a direcao pq o padrao do sprite do inimigo é olhando para a esquerda
+            UpdateLocalScale(new Vector3(-this.realDirection, 0, 0));
+        }
+        
+        transform.position = newPosition;
+        
+        
+    }
+
+    private void UpdateLocalScale(Vector3 movement) {
+        transform.localScale = new Vector3(Mathf.Sign(movement.x) * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.layer == LayerMask.NameToLayer("PlayerRedirector")){
+            this.realDirection = col.gameObject.GetComponent<EnemyRedirectioner>().getAngle();
+        }
+    }
+
+    public void Reset() {
+		transform.position = initialPosition;
+        if(InitialDirection == Direction.Right)realDirection = 1;
+        if(InitialDirection == Direction.Left)realDirection = -1;
+        if(InitialDirection == Direction.Stoped)realDirection = 0;
+	}
+}
