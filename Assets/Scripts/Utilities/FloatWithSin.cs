beb57c42830214989a8c86d379e104609879c711
Guilherme Oliveira <guilherme.061@gmail.com>
2019-05-27 12:35:04 -0400

    testando script de floating no mapa

--- Assets/Scripts/Utilities/FloatWithSin.cs
+++ Assets/Scripts/Utilities/FloatWithSin.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class FloatWithSin : MonoBehaviour {
 	
 	Vector3 originalPosition;
-	float angle = 0f;
+	private float m_angle = 0f;
+    private float m_speed = 1f;
+    private float m_speedDivisor = 8f;
 
 	void Start() {
 		originalPosition = transform.position;
+        m_angle = Random.Range(0, 360);
+        m_speed = Random.Range(0.1f, 2.0f);
+        m_speedDivisor = Random.Range(2.0f, 8.0f);
 	}
 
 	void Update() {
-		angle += Time.deltaTime;
-		transform.position = new Vector3(originalPosition.x, originalPosition.y + (Mathf.Sin(angle) / 8), originalPosition.z);
+		m_angle += (m_speed * Time.deltaTime);
+		transform.position = new Vector3(originalPosition.x, originalPosition.y + (Mathf.Sin(m_angle) / m_speedDivisor), originalPosition.z);
 	}
 }
