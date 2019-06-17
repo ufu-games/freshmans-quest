4c5a0b97036bfb2a339776e3afeacb28932e08ea
Guilherme Oliveira <guilherme.061@gmail.com>
2019-06-16 20:37:11 -0400

    reestruturando o pulo do player

--- Assets/Scripts/Player Controller/PlayerController.cs
+++ Assets/Scripts/Player Controller/PlayerController.cs
@@ -4,24 +4,31 @@ using Cinemachine;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	
-	// Movement Handling
-	private const float runSpeed = 8.5f;
-	private const float groundDamping = .25f; // how fast do we change direction? higher means faster
+
+    [Header("Jumping Parameters")]
+    public float runSpeed = 7.5f;
+    public float jumpPeakHeight = 4.5f;
+    public float horizontalDistanceToJumpPeak = 4.5f;
+
+    [Header("Environment Parameters")]
+    public float jumpingPlatformJumpingVelocityMultiplier = 1.25f;
+
+    // Movement Handling
+    private const float groundDamping = .25f; // how fast do we change direction? higher means faster
 	private const float slippingFrictionMultiplier = .1f;
-	
-	// Jump Handling
-	private const float goingUpGravity = -35f;
-	private const float goingDownGravity = -69;
+
+    // Jump Handling
+    [ReadOnly]
+    private float m_gravity;
+    private float m_goingUpGravity;
+	private float m_goingDownGravity;
+    private float m_jumpInitialVelocity;
+
 	private const float terminalVelocity = -20f;
-	[ReadOnly]
-	private float m_gravity;
 	private const float inAirDamping = 0.08333f;
-	private const float jumpHeight = 2.6f;
-	private const float jumpPressedRememberTime = 0.1f;
-	private const float groundedRememberTime = 0.1f;
+	private const float jumpPressedRememberTime = 0.125f;
+	private const float groundedRememberTime = 0.125f;
 	private const float cutJumpHeight = 0.35f;
-	private float nextJumpHeight = 2f;
 
 	private float m_jumpPressedRemember;
 	private float m_groundedRemember;
@@ -84,18 +91,23 @@ public class PlayerController : MonoBehaviour {
 	private const float petTheDogDistance = 0.1f;
 
 	void Awake() {
-		m_animator = GetComponentInChildren<Animator>();
-		m_playerSprite = m_animator.transform;
+        // Calculating Gravity and Jump Initial Velocity...
+        m_jumpInitialVelocity = (2 * jumpPeakHeight * runSpeed) / (horizontalDistanceToJumpPeak);
+        m_goingUpGravity = -((2 * jumpPeakHeight * runSpeed * runSpeed) / (horizontalDistanceToJumpPeak * horizontalDistanceToJumpPeak));
+        m_goingDownGravity = m_goingUpGravity * 2.75f;
+        m_gravity = m_goingUpGravity;
 
+        // Caching Components
+        m_animator = GetComponentInChildren<Animator>();
+		m_playerSprite = m_animator.transform;
 		m_controller = GetComponent<Prime31.CharacterController2D>();
 		m_controller.onControllerCollidedEvent += onControllerCollider;
 		m_controller.onTriggerEnterEvent += onTriggerEnterEvent;
 		m_controller.onTriggerStayEvent += OnTriggerStayEvent;
 		m_controller.onTriggerExitEvent += onTriggerExitEvent;
-		
 		m_originalScale = m_playerSprite.localScale;
 		
-		m_gravity = goingUpGravity;
+		
 
         if(Camera.main.GetComponentInChildren<CinemachineVirtualCamera>()) {
 			m_cam = Camera.main.GetComponentInChildren<CinemachineFramingTransposer>();
@@ -221,8 +233,8 @@ public class PlayerController : MonoBehaviour {
 		}
 
 		if(col.gameObject.layer == LayerMask.NameToLayer("JumpingPlatform")) {
-			m_gravity = goingUpGravity;
-			m_velocity.y = Mathf.Sqrt( 2f * jumpHeight * col.GetComponent<JumpingPlatform>().jumpingMultiplier * -m_gravity );
+			m_gravity = m_goingUpGravity;
+			m_velocity.y = jumpingPlatformJumpingVelocityMultiplier * m_jumpInitialVelocity;
 			
 			if(SoundManager.instance && SoundManager.instance.Settings.Player_jump != "") {
 				SoundManager.instance.PlaySfx(SoundManager.instance.Settings.Player_jump);
@@ -268,7 +280,7 @@ public class PlayerController : MonoBehaviour {
 	public void getsThrownTo(float rotationAngle, float maxVelocity){
 		m_jumpPressedRemember = 0;
 		m_groundedRemember = 0;
-		m_gravity = goingUpGravity;
+		m_gravity = m_goingUpGravity;
 
 		m_velocity.y = Mathf.Cos(rotationAngle * Mathf.Deg2Rad) * maxVelocity;
 		m_velocity.x = -Mathf.Sin(rotationAngle * Mathf.Deg2Rad) * maxVelocity;
@@ -289,14 +301,14 @@ public class PlayerController : MonoBehaviour {
 	{	
 		m_groundedRemember -= Time.deltaTime;
 		m_jumpPressedRemember -= Time.deltaTime;
+        normalizedHorizontalSpeed = Input.GetAxisRaw("Horizontal");
 
-		SaveSystem.instance.TickTimePlayed();
+        SaveSystem.instance.TickTimePlayed();
 
 		// for some reason this wasn't working on the ProcessNormalState()
 		if(m_controller.isGrounded) {
 			m_isOnWall = false;
 			m_groundedRemember = groundedRememberTime;
-			m_gravity = goingUpGravity;
 			m_velocity.y = 0;
 			
 			// wasn't grounded last frame
@@ -347,40 +359,29 @@ public class PlayerController : MonoBehaviour {
 		}
 
 		AnimationLogic();
-		m_velocityLastFrame = m_velocity;
+        ProcessSpriteScale();
 		CamHandling();
+        m_velocityLastFrame = m_velocity;
 
 		// Handling Player Velocity and Moving
-
 		// Horizontal Velocity
 		float t_groundDamping = m_isSlipping ? (groundDamping * slippingFrictionMultiplier) : groundDamping;
 		var smoothedMovementFactor = m_controller.isGrounded ? t_groundDamping : inAirDamping;
-		m_velocity.x = Mathf.Lerp(normalizedHorizontalSpeed * runSpeed, m_velocity.x,Mathf.Pow(1 - smoothedMovementFactor, Time.deltaTime*60));
-		
-		// Vertical Velocity
-		// ACCELERATING with gravity
-		float thisFrameYVelocity = m_velocity.y + (m_gravity * Time.deltaTime);
-		m_velocity.y = Mathf.Max(terminalVelocity, thisFrameYVelocity);
+		// m_velocity.x = Mathf.Lerp(normalizedHorizontalSpeed * runSpeed, m_velocity.x , Mathf.Pow(1 - smoothedMovementFactor, Time.deltaTime*60));
+        m_velocity.x = Mathf.Lerp(m_velocity.x, normalizedHorizontalSpeed * runSpeed, Mathf.Pow(1 - smoothedMovementFactor, Time.deltaTime * 60));
+        // Vertical Velocity
+        // ACCELERATING with gravity
+        m_velocity.y += m_gravity * Time.deltaTime;
+		m_velocity.y = Mathf.Max(terminalVelocity, m_velocity.y);
 		
 		Vector2 eulerDeltaPosition = m_velocity * Time.deltaTime;
-		// Velocity Verlet giving bad results
-		// Vector2 velocityVerletDeltaPosition = new Vector2(
-		// 	(m_velocity.x * Time.deltaTime) + (.5f * normalizedHorizontalSpeed * Time.deltaTime * Time.deltaTime), 
-		// 	(m_velocity.y * Time.deltaTime) + (.5f * m_gravity * Time.deltaTime * Time.deltaTime)
-		// );
+		Vector2 velocityVerletDeltaPosition = new Vector2(
+		 	(eulerDeltaPosition.x), (eulerDeltaPosition.y) + (.5f * m_gravity * Time.deltaTime * Time.deltaTime)
+		 );
 
 		if(!m_skipMoveOnUpdateThisFrame) {
-			m_controller.move( eulerDeltaPosition );
+			m_controller.move(velocityVerletDeltaPosition);
 			m_velocity = m_controller.velocity;
-		}
-		
-		// ignora as "one way platforms" por um frame (para cair delas)
-		// ISSO AQUI DÁ UM BUG
-		// SE VC SEGURAR PRA BAIXO E PULAR, O PERSONAGEM DÁ UM PULÃO
-		// A PARTIR DE AGORA ISSO É UMA FEATURE
-		if( m_controller.isGrounded && Input.GetKey( KeyCode.DownArrow ) ) {
-			m_velocity.y *= Mathf.Pow(3f,Time.deltaTime);
-			m_controller.ignoreOneWayPlatformsThisFrame = true;
 		}
 
 		m_skipMoveOnUpdateThisFrame = false;
@@ -411,27 +412,41 @@ public class PlayerController : MonoBehaviour {
 
 	#region Processing States
 	private void ProcessNormalState() {
-		Move();
-		Jump();
-	}
+        // REGULAR JUMP
+        if (((m_groundedRemember > 0) && (m_jumpPressedRemember > 0))) {
+            m_jumpPressedRemember = 0;
+            m_groundedRemember = 0;
+            m_jumpedLastFrame = true;
+            SaveSystem.instance?.Jumped();
+
+            m_gravity = m_goingUpGravity;
+            m_velocity.y = m_jumpInitialVelocity;
+
+            if (SoundManager.instance && SoundManager.instance.Settings.Player_jump != "") {
+                SoundManager.instance.PlaySfx(SoundManager.instance.Settings.Player_jump);
+            }
+
+            StartCoroutine(ChangeScale(m_playerSprite.localScale * m_goingUpScaleMultiplier));
+            m_currentPlayerState = EPlayerState.Jumping;
+        }
+    }
 
 	private void ProcessJumpingState() {
-		Move();
 		WallJump();
 
 		// Cutting Jump
 		if(InputManager.instance.ReleasedJump()) {
 			if(m_velocity.y > 0) {
-				m_velocity.y = m_velocity.y * Mathf.Pow(cutJumpHeight,Time.deltaTime*60);
+				m_velocity.y *= Mathf.Pow(cutJumpHeight,Time.deltaTime*60);
 			}
 		}
 
 		// Changing Gravity on Fall
 		if(m_velocity.y < 0) {
 			if(!m_isOnWall) {
-				m_gravity = goingDownGravity;
+				m_gravity = m_goingDownGravity;
 			} else {
-				m_gravity = goingUpGravity;
+				m_gravity = m_goingUpGravity;
 			}
 		}
 
@@ -441,7 +456,9 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	private void ProcessDialogueState() {
-		if(!DialogueManager.instance.isShowingDialogue) m_currentPlayerState = EPlayerState.Normal;
+        if (!DialogueManager.instance.isShowingDialogue) {
+            m_currentPlayerState = EPlayerState.Normal;
+        }
 	}
 
 	private void ProcessPettingState() {
@@ -476,10 +493,7 @@ public class PlayerController : MonoBehaviour {
 	}
 	#endregion
 
-	private void Move() {
-		float horizontalMovement = Input.GetAxisRaw("Horizontal");
-		normalizedHorizontalSpeed = horizontalMovement;
-
+	private void ProcessSpriteScale() {
 		if(Mathf.Abs(normalizedHorizontalSpeed) > Mathf.Epsilon ||
 			Mathf.Abs(m_controller.velocity.x) > 0.05f) {
 				// tem que chegar por um valor relativamente alto no controller porque
@@ -489,27 +503,6 @@ public class PlayerController : MonoBehaviour {
 				if(!m_isOnWall) {
 					m_playerSprite.localScale = new Vector3(sign * Mathf.Abs(m_playerSprite.localScale.x), m_playerSprite.localScale.y, m_playerSprite.localScale.z);
 				}
-		}
-	}
-
-	private void Jump() {
-		// REGULAR JUMP
-		if( ( (m_groundedRemember > 0) && (m_jumpPressedRemember > 0) ) ) {
-			m_jumpPressedRemember = 0;
-			m_groundedRemember = 0;
-			m_jumpedLastFrame = true;
-			SaveSystem.instance.Jumped();
-			m_gravity = goingUpGravity;
-			m_velocity.y = Mathf.Sqrt( 2f * nextJumpHeight * -m_gravity );
-			nextJumpHeight = jumpHeight;
-			
-			if(SoundManager.instance && SoundManager.instance.Settings.Player_jump != "") {
-				SoundManager.instance.PlaySfx(SoundManager.instance.Settings.Player_jump);
-			}
-
-			StartCoroutine(ChangeScale(m_playerSprite.localScale * m_goingUpScaleMultiplier));
-			// [CHANGING STATE]
-			m_currentPlayerState = EPlayerState.Jumping;
 		}
 	}
 
@@ -546,7 +539,7 @@ public class PlayerController : MonoBehaviour {
 
 			m_isOnWall = false;
 			m_velocity.x = wallJumpVelocity.x * (m_controller.isNear(Vector2.left,maxDistanceOffWall) ? 1 : -1);
-			m_gravity = goingUpGravity;
+			m_gravity = m_goingUpGravity;
 			m_velocity.y = Mathf.Sqrt(wallJumpVelocity.y * -m_gravity);
 			
 			SaveSystem.instance.Jumped();
@@ -578,6 +571,8 @@ public class PlayerController : MonoBehaviour {
 
 	private void CamHandling(){
 		return;
+
+        // nice
 		if(!m_cam) return;
 
 		if(m_controller.isGrounded) {
